1cba29e7b3f37e4cadf33996d32e0085dbcc47fb
Matheus <matheusbrito174@gmail.com>
2022-04-17 19:14:53 -0300

    Corrigindo nome da interface NegociacaoDoDia.

--- app/src/interfaces/negociacao-do-dia.ts
+++ app/src/interfaces/negociacao-do-dia.ts
@@ -1,4 +1,4 @@
-export interface NegociacoesDoDia {
+export interface NegociacaoDoDia {
     montante: number;
     vezes: number;
 }
--- app/src/services/negociacoes-service.ts
+++ app/src/services/negociacoes-service.ts
@@ -1,4 +1,4 @@
-import { NegociacoesDoDia } from "../interfaces/negociacoes-do-dia.js";
+import { NegociacaoDoDia } from "../interfaces/negociacao-do-dia.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 
@@ -6,7 +6,7 @@ export class NegociacoesService {
     public buscarNegociacoesDoDia(): Promise<Negociacoes> {
         return fetch('http://localhost:8080/dados')
             .then((dados: Response) => dados.json())
-            .then((dados: NegociacoesDoDia[]) => dados.map(
+            .then((dados: NegociacaoDoDia[]) => dados.map(
                 dado => new Negociacao(new Date(), dado.vezes, dado.montante)
             ))
             .then((negociacoes: Negociacao[]) => new Negociacoes(negociacoes));
